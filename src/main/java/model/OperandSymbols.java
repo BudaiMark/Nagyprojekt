651cb4ac8d23620b6360da9cd240421e31e3a7a0
Budai Márk <budaimark00@gmail.com>
2021-11-14 11:30:45 +0100

    Calculate enum calss ready

---
+++ src/main/java/model/OperandSymbols.java
@@ -0,0 +1,9 @@
+package model;
+public enum OperandSymbols {
+    SUM,
+    SUBTRACTION,
+    MULTPLY,
+    ALLOPERATION
+
+
+}
---
+++ src/main/java/model/Operation.java
@@ -0,0 +1,40 @@
+package model;
+import java.util.Random;
+
+public class Operation {
+    private int leftoperand;
+    private int rightoperand;
+    private OperandSymbols operandsymbol;
+
+    public Operation(OperandSymbols operationsymbol) {
+        Random random = new Random();
+        this.leftoperand = random.nextInt(10);
+        this.rightoperand = random.nextInt(10);
+        this.operandsymbol = operationsymbol;
+        calculate(leftoperand, rightoperand, operationsymbol);
+
+
+    }
+
+    public int calculate(int leftoperand, int rightoperand, OperandSymbols operationsymbol) {
+        int returnvalue = 0;
+        switch (operandsymbol) {
+            case SUM:
+                returnvalue = leftoperand + rightoperand;
+                break;
+            case SUBTRACTION:
+                returnvalue = leftoperand - rightoperand;
+                break;
+            case MULTPLY:
+                returnvalue = leftoperand * rightoperand;
+            case ALLOPERATION:
+                Random random = new Random();
+                OperandSymbols tempsymbol = OperandSymbols.values()[random.nextInt(2)];
+                returnvalue = calculate(leftoperand, rightoperand, tempsymbol);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + operandsymbol);
+        }
+        return returnvalue;
+    }
+}
